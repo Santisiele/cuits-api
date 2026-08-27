68966f089e7973e45d2350265836539e427aaa27
Santi Siele <santi.siele@gmail.com>
2026-08-27 13:02:08 -0300

    remove duplicated methods in graph repository port

--- src/ports/interfaces.ts
+++ src/ports/interfaces.ts
@@ -79,18 +79,6 @@ export interface IGraphRepository {
    * @param category - "known" (default) or "to_know". Additive: re-loading
    *                   an existing node never clears the other flag.
    */
-  findBirthdaysBetween(
-    fromMonth: number,
-    fromDay: number,
-    toMonth: number,
-    toDay: number
-  ): Promise<BirthdayResult[]>
-
-  // Writes
-  updateNode(taxId: string, fields: CuitNodeUpdate): Promise<UpdateNodeResult>
-  addRelationship(fromTaxId: string, toTaxId: string, relationshipType: string): Promise<AddRelationshipResult>
-  deleteRelationship(fromTaxId: string, toTaxId: string, relationshipType: string): Promise<DeleteRelationshipResult>
-
   // Ingestion (used by LoaderService)
   upsertBaseNode(
     taxId: string,
